5b2b918d2459a970f61aa53892f0f9382ec16ba5
Benjamin Elliot <benjaminelliot@users.noreply.github.com>
2026-03-05 11:23:52 -0500

    Add more treesitter bindings

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -20,8 +20,23 @@ return {
         'nvim-treesitter/nvim-treesitter-textobjects',
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
         config = function()
-            -- No setup needed — just ensure its queries/ dir is in runtimepath
+            -- Ensure its queries/ dir is in runtimepath
             vim.opt.runtimepath:append(vim.fn.stdpath('data') .. '/lazy/nvim-treesitter-textobjects')
+
+            local move = require('nvim-treesitter-textobjects.move')
+            local maps = {
+                { key = ']f', fn = move.goto_next_start,     arg = '@function.outer', desc = 'Next function start' },
+                { key = ']a', fn = move.goto_next_start,     arg = '@parameter.inner', desc = 'Next parameter' },
+                { key = ']c', fn = move.goto_next_start,     arg = '@class.outer',    desc = 'Next class start' },
+                { key = ']F', fn = move.goto_next_end,       arg = '@function.outer', desc = 'Next function end' },
+                { key = '[f', fn = move.goto_previous_start, arg = '@function.outer', desc = 'Prev function start' },
+                { key = '[a', fn = move.goto_previous_start, arg = '@parameter.inner', desc = 'Prev parameter' },
+                { key = '[c', fn = move.goto_previous_start, arg = '@class.outer',    desc = 'Prev class start' },
+                { key = '[F', fn = move.goto_previous_end,   arg = '@function.outer', desc = 'Prev function end' },
+            }
+            for _, m in ipairs(maps) do
+                vim.keymap.set('n', m.key, function() m.fn(m.arg) end, { desc = m.desc })
+            end
         end,
     },
 }
